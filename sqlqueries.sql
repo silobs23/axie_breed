71f8ebc0b01e93e8b21dc43b8e158d7124c92176
silobs23 <93286886+silobs23@users.noreply.github.com>
2021-11-02 20:22:22 -0700

    Update and rename SQLQueries.sql to sqlqueries.sql

--- sqlqueries.sql
+++ sqlqueries.sql
@@ -1,6 +1,3 @@
->> SQL Queries
-
-
 select backd, hornd, mouthd, taild, class, avg(price) as avgprice, count(mouthd) as sales from sales
 group by backd, hornd, mouthd, taild, class
 having avg(price) >= 0.11
