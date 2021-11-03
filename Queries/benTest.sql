376bb37647adc38965392eddf9def7db0611a6fc
bkennady <bengerginger@gmail.com>
2021-11-02 21:55:50 -0500

    updated

--- Queries/benTest.sql
+++ Queries/benTest.sql
@@ -1,3 +1,3 @@
-select backd, class, AVG(price) as avgprice, count(backd) as sales from sales
-group by backd, class
+select backd, hornd, mouthd, taild, class, AVG(price) as avgprice, count(backd) as sales from sales
+group by backd, hornd, mouthd, taild, class
 order by avgprice desc
