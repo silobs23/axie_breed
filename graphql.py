f7c458607f6960cf1befffed0cfeffa68f9f10e4
silobs23 <93286886+silobs23@users.noreply.github.com>
2021-11-07 14:25:37 -0800

    Update graphql.py

--- graphql.py
+++ graphql.py
@@ -1,85 +1,9 @@
 import datetime
 from datetime import datetime
-import json
 import requests
 import psycopg2
-from urllib.request import urlopen
+import time
 import json
-from psycopg2.extras import Json
-
-# query
-testQuery = """
-query GetRecentlyAxiesSold($from: Int, $size: Int) {
-  settledAuctions {
-    axies(from: $from, size: $size) {
-      total
-      results {
-        ...AxieSettledBrief
-        transferHistory {
-          ...TransferHistoryInSettledAuction
-          __typename
-        }
-        __typename
-      }
-      __typename
-    }
-    __typename
-  }
-}
-fragment AxieSettledBrief on Axie {
-  id
-  class
-  parts {
-    ...AxiePart
-    __typename
-  }
-  stats {
-    ...AxieStats
-    __typename
-  }
-}
-fragment AxiePart on AxiePart {
-  id
-}
-fragment AxieStats on AxieStats {
-  hp
-  speed
-  skill
-  morale
-  __typename
-}
-fragment TransferHistoryInSettledAuction on TransferRecords {
-  total
-  results {
-    ...TransferRecordInSettledAuction
-    __typename
-  }
-  __typename
-}
-fragment TransferRecordInSettledAuction on TransferRecord {
-  timestamp
-  withPrice
-  withPriceUsd
-}
-"""
-
-# post request
-post = {
-    "operationName": "GetRecentlyAxiesSold",
-    "variables": {
-        "from": 0,
-        "size": 100
-    },
-    "query": testQuery,
-}
-
-# Make query
-url = "https://graphql-gateway.axieinfinity.com/graphql"
-r = requests.post(url, post)
-res = r.json()
-# write to a file -- THIS IS FOR SCRIPT BEHAVIOR CONFIRMATION. DELETE WHEN NO LONGER NECESSARY
-with open('axiedata.json', 'w') as f:
-    json.dump(res, f)
 
 
 # Parse through the response JSON and store important values in a dictionary
@@ -153,33 +77,114 @@ def get_axie_data(dictionary):
 
     print("length of unique AxieID in response: ", len(axies_list))
     print("Total rows added added:", i)
+    return i
 
 
 # Connect to database and add to table
 def execute_sql(axie_sales_dict, db_conn):
-    # create cursor
-    cur = db_conn.cursor()
+    try:
+        # create cursor
+        cur = db_conn.cursor()
 
-    cols = axie_sales_dict.keys()
-    cols_str = ", ".join(cols)
+        cols = axie_sales_dict.keys()
+        cols_str = ", ".join(cols)
 
-    vals = [str(axie_sales_dict[x]) for x in cols]
-    vals_str_list = ["%s"] * len(vals)
-    vals_str_actual = ", ".join(vals)
-    vals_str = ", ".join(vals_str_list)
+        vals = [str(axie_sales_dict[x]) for x in cols]
+        vals_str_list = ["%s"] * len(vals)
+        vals_str = ", ".join(vals_str_list)
 
-    # Generate SQL
-    sql_string = f"INSERT INTO sales ({cols_str}) VALUES ({vals_str})"
+        # Generate SQL
+        sql_string = f"INSERT INTO sales ({cols_str}) VALUES ({vals_str})"
+        # execute sql
+        cur.execute(sql_string, vals)
+        db_conn.commit()
+        cur.close()
+    except Exception as e:
+        print(f"Duplicate warning!!!: {e}")
+        db_conn.rollback()
+        cur.close()
 
-    # exectue sql
-    cur.execute(sql_string, vals)
-    db_conn.commit()
 
-    cur.close()
-
+m = 0
 
 # Run script
-print("Running script... \n")
-get_axie_data(res)
-print("\n-------------------------")
-print("DONE MOTHER FUCKER")
+while m < 5000:
+    # query
+    testQuery = """
+query GetRecentlyAxiesSold($from: Int, $size: Int) {
+settledAuctions {
+axies(from: $from, size: $size) {
+  total
+  results {
+    ...AxieSettledBrief
+    transferHistory {
+      ...TransferHistoryInSettledAuction
+      __typename
+    }
+    __typename
+  }
+  __typename
+}
+__typename
+}
+}
+fragment AxieSettledBrief on Axie {
+id
+class
+parts {
+...AxiePart
+__typename
+}
+stats {
+...AxieStats
+__typename
+}
+}
+fragment AxiePart on AxiePart {
+id
+}
+fragment AxieStats on AxieStats {
+hp
+speed
+skill
+morale
+__typename
+}
+fragment TransferHistoryInSettledAuction on TransferRecords {
+total
+results {
+...TransferRecordInSettledAuction
+__typename
+}
+__typename
+}
+fragment TransferRecordInSettledAuction on TransferRecord {
+timestamp
+withPrice
+withPriceUsd
+}
+"""
+    # post request
+    post = {
+"operationName": "GetRecentlyAxiesSold",
+"variables": {
+    "from": 0,
+    "size": 100
+},
+"query": testQuery,
+}
+    # Make query
+    url = "https://graphql-gateway.axieinfinity.com/graphql"
+    r = requests.post(url, post)
+    res = r.json()
+    # write to a file -- THIS IS FOR SCRIPT BEHAVIOR CONFIRMATION. DELETE WHEN NO LONGER NECESSARY
+    with open('axiedata.json', 'w') as f:
+        json.dump(res, f)
+
+    print("Running script... \n")
+    n = get_axie_data(res)
+    m += n
+    print("\n-------------------------\n\n")
+    print(f"TOTAL ROWS ADDED: {m} , THESE VALUES ARE INCORRECT WHEN DUPLICATES ARE FOUND. NOT RELIABLE\n\n")
+    print("--------------------------\n\n\n\n\n\n")
+    time.sleep(30)
