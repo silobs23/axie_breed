a542b39d4036e97799e3eedfba61fa8d11ba0b50
Keyur <93286886+silobs23@users.noreply.github.com>
2022-03-21 07:49:40 -0700

    Delete sqlqueries.sql

--- sqlqueries.sql
+++
@@ -1,7 +0,0 @@
-select backd, hornd, mouthd, taild, class, avg(price) as avgprice, count(mouthd) as sales from sales
-group by backd, hornd, mouthd, taild, class
-having avg(price) >= 0.11
-order by sales desc
-
-
-
